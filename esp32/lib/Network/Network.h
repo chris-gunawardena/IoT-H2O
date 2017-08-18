bd8c60188bdfe204aa7e2b395c0bc0d76714ab37
Chris Gunawardena <chris.gun@nl.ibm.com>
2017-08-19 00:00:46 +0200

    refactor

---
+++ esp32/lib/Network/Network.h
@@ -0,0 +1,98 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <Arduino.h>
+
+class Network
+{
+  public:
+    Network(void (*sleep)());
+    void wifi_init();
+    void wifi_connect();
+    void mqtt_init();
+    void mqtt_connect();
+    void mqtt_send(int reading, MQTT_CALLBACK_SIGNATURE);
+    void mqtt_loop();
+    String get_reply_id();
+
+private:
+    const char* ssid = "FASTWEB-1-D2700B";
+    const char* password = "978C3B413C";
+    const char* mqtt_server = "mqtt.chris.gunawardena.id.au";
+    const long mqtt_reply_timeout_ms = 5 * 1000;
+    const size_t bufferSize = JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(5) + 180;    
+    const char* request_template = "{\"uri\":\"https://water-9dbfa.firebaseio.com/users/chris/readings.json\",\"method\":\"POST\",\"json\":true,\"body\":{\"level\":123456,\"timestamp\":{\".sv\":\"timestamp\"}},\"reply_id\":\"/request/reply/123456\"}";
+    String reply_id;
+    WiFiClient wiFiClient;
+    PubSubClient pubSubClient;
+    unsigned long request_start_time;
+    void (*_sleep)();
+};
+
+Network::Network(void (*sleep)()) {
+    _sleep = sleep;
+
+}
+
+String Network::get_reply_id() {
+    return reply_id;
+}
+
+void Network::wifi_connect() {
+    Serial.println("Connecting to WiFi");
+    int wifi_rety_count = 10;
+    WiFi.begin(ssid, password); 
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.println(".");
+      if(--wifi_rety_count < 0) {
+        Serial.println("ERROR: No wifi connection in " + String(wifi_rety_count) + " tries, sleeping.");
+        (*_sleep)();
+      }
+      delay(1000);
+    }
+    Serial.println("WiFi connected!");
+}
+
+void Network::mqtt_connect() {
+    Serial.println("Connecting to MQTT");
+
+    int mqtt_rety_count = 10;
+    pubSubClient.setClient(wiFiClient);
+    pubSubClient.setServer(mqtt_server, 1883);
+
+    while (!pubSubClient.connected()) {
+        String clientId = "esp32client" + String(random(0xffff), HEX);
+        reply_id = "/request/reply/" + clientId;
+        // if (client.connect(clientId.c_str(), mqtt_user.c_str(), mqtt_password.c_str())) {
+        if (pubSubClient.connect(clientId.c_str())) {
+          Serial.println("MQTT connected");
+        } else {
+          Serial.print("ERROR: mqtt failed: ");
+          Serial.print(pubSubClient.state());
+          Serial.println(" MQTT try again in 1 second");
+          delay(1000);
+          if(--mqtt_rety_count < 0) {
+            Serial.println("ERROR: No MQTT connection in " + String(mqtt_rety_count) + " tries, sleeping.");
+            (*_sleep)();
+          }  
+        }
+    }
+}
+void Network::mqtt_loop() {
+    pubSubClient.loop();
+}
+
+void Network::mqtt_send(int reading, MQTT_CALLBACK_SIGNATURE) {
+    pubSubClient.setCallback(callback);    
+    DynamicJsonBuffer jsonBuffer(bufferSize);    
+    JsonObject& root = jsonBuffer.parseObject(request_template);
+    root["reply_id"] = reply_id;
+    pubSubClient.subscribe(reply_id.c_str());
+    root["body"]["level"] = reading;
+    String request;
+    root.printTo(request);
+    Serial.println(request.c_str());
+    pubSubClient.publish("/request", request.c_str());
+    request_start_time = millis();
+}
+
--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,34 +1,16 @@
 #include <Arduino.h>
-#include <WiFi.h>
 #include <esp_log.h>
-#include <PubSubClient.h>
-#include <ArduinoJson.h>
+#include <Measurement.h>
+#include <Network.h>
 
-const char* ssid = "FASTWEB-1-D2700B";
-const char* password = "978C3B413C";
-const char* mqtt_server = "mqtt.chris.gunawardena.id.au";
-RTC_DATA_ATTR int bootCount = 1;
 RTC_DATA_ATTR int last_reading = 0;
 const int a_in_pin = 34;
 const int led_pin = 5;
 int sleep_time_seconds = 1 * 60;
-const long mqtt_timeout_ms = 5 * 1000;
-const size_t bufferSize = JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(5) + 180;
-DynamicJsonBuffer jsonBuffer(bufferSize);
-const char* request_template = "{\"uri\":\"https://water-9dbfa.firebaseio.com/users/chris/readings.json\",\"method\":\"POST\",\"json\":true,\"body\":{\"level\":123456,\"timestamp\":{\".sv\":\"timestamp\"}},\"reply_id\":\"/request/reply/123456\"}";
+void sleep();
+Network network(sleep);
 String reply_id;
-WiFiClient wiFiClient;
-PubSubClient pubSubClient(wiFiClient);
-unsigned long request_start_time;
 
-void sleep() {
-  esp_deep_sleep_enable_timer_wakeup(sleep_time_seconds * 1000000);
-  esp_deep_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
-  esp_deep_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
-  esp_deep_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
-  esp_deep_sleep_pd_config(ESP_PD_DOMAIN_MAX, ESP_PD_OPTION_OFF);
-  esp_deep_sleep_start();  
-}
 
 void mqtt_callback(char* topic, byte* payload, unsigned int length) {
   Serial.println(topic);
@@ -37,8 +19,7 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 
-  if (strcmp(topic, reply_id.c_str()) == 0) {
-    digitalWrite(led_pin, LOW);
+  if (strcmp(topic, network.get_reply_id().c_str()) == 0) {
     sleep();
   }
 }
@@ -53,55 +34,33 @@ void setup(){
   digitalWrite(led_pin, HIGH);
 
   // measure 
-  int reading = analogRead(a_in_pin);
+  Messurement messurement(a_in_pin);
+  int reading = messurement.get_level();
   Serial.println("Reading: " + String(reading));
 
-  if (reading != last_reading && reading != 0) {
-    // WiFi
-    WiFi.begin(ssid, password); 
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(5000);
-      Serial.println("Connecting to WiFi..");
-    }
-    Serial.println("Connected to the WiFi network");
-
-    // MQTT
-    pubSubClient.setServer(mqtt_server, 1883);
-    pubSubClient.setCallback(mqtt_callback);
-
-    while (!pubSubClient.connected()) {
-      String clientId = "esp32client" + String(random(0xffff), HEX);
-      reply_id = "/request/reply/" + clientId;
-      // if (client.connect(clientId.c_str(), mqtt_user.c_str(), mqtt_password.c_str())) {
-      if (pubSubClient.connect(clientId.c_str())) {
-        Serial.println("mqtt connected");
-        JsonObject& root = jsonBuffer.parseObject(request_template);
-        root["reply_id"] = reply_id;
-        pubSubClient.subscribe(reply_id.c_str());
-        root["body"]["level"] = reading;
-        String request;
-        root.printTo(request);
-        Serial.println(request.c_str());
-        pubSubClient.publish("/request", request.c_str());
-        request_start_time = millis();
-        last_reading = reading;
-      } else {
-        Serial.print("mqtt failed, rc=");
-        Serial.print(pubSubClient.state());
-        Serial.println("mqtt try again in 5 seconds");
-        delay(5000);
-      }
-    }
+  if (reading == last_reading || reading == 0) {
+    sleep();
+  } else {
+    network.wifi_connect();
+    network.mqtt_connect();
+    network.mqtt_send(reading, mqtt_callback);
   }
 }
 
 void loop() {
-  pubSubClient.loop();
-  if ((millis() - request_start_time) > mqtt_timeout_ms) {
-    sleep();
-  }
+  // Serial.println(analogRead(a_in_pin));  
+  network.mqtt_loop();
 }
 
+void sleep() {
+  digitalWrite(led_pin, LOW);
+  esp_deep_sleep_enable_timer_wakeup(sleep_time_seconds * 1000000);
+  esp_deep_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+  // esp_deep_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
+  // esp_deep_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
+  // esp_deep_sleep_pd_config(ESP_PD_DOMAIN_MAX, ESP_PD_OPTION_OFF);
+  esp_deep_sleep_start();  
+}
 
 // void log_memory() {
 //   UBaseType_t uxHighWaterMark2 = uxTaskGetStackHighWaterMark( NULL );
